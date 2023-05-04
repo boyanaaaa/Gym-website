209f802e8e56e6d5db14d3cf16a76345d2f00214
Boyana Dimitrova <boyanadimitrova11@gmail.com>
2023-05-04 08:52:50 +0300

    Final updates

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 import Logo from '../../assets/gym1.png'
+import Menu from '../../assets/menu.png'
+import {Link} from "react-scroll";
 
 const Header = () => {
+    const mobile = window.innerWidth<=768 ? true : false;
+    const [menuOpened, setMenuOpened] = useState(false);
     return (
-        <div className="header">
+        <div className="header"> 
             <img src={Logo} className="logo" />
-            <ul className="header-menu">
-                <li>Home</li>
-                <li>Programs</li>
-                <li>Why us</li>
-                <li>Plans</li>
-            </ul>
+            {menuOpened === false && mobile ===true? (
+                <div onClick={()=>setMenuOpened(true)}><img src={Menu} style ={ { width: '2.5rem', height: '2.5rem'}} /></div>
+            ) : (<ul className="header-menu">
+            
+            <li ><Link onClick={() => setMenuOpened(false)} to="hero" span={true} smooth={true}>Home</Link></li>
+            <li ><Link onClick={() => setMenuOpened(false)} to="programs" span={true} smooth={true}>Programs</Link></li>
+            <li ><Link onClick={() => setMenuOpened(false)} to="reasons" span={true} smooth={true}>Why Us?</Link></li>
+            <li ><Link onClick={() => setMenuOpened(false)} to="plans" span={true} smooth={true}>Plans</Link></li>
+            <li ><Link onClick={() => setMenuOpened(false)} to="testimonials" span={true} smooth={true}>Testimonials</Link></li>
+        </ul>)}
+            
         </div>
     )
 }
